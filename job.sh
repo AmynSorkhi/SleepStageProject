12c06d9c60056d9cc4b2c1da2ebf65c0b119f710
Amyn Sorkhi <amynsorkhi@gmail.com>
2025-04-16 19:24:49 -0400

    Update job.sh

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=3CNN        # Job name
 #SBATCH --output=output_%j.log            # Output file (%j = job ID)
 #SBATCH --error=error_%j.log              # Error file (%j = job ID)
-#SBATCH --time=00:30:00                   # Time limit (hh:mm:ss)
+#SBATCH --time=24:00:00                   # Time limit (hh:mm:ss)
 #SBATCH --mem=128G                         # Memory per node
 #SBATCH --gres=gpu:4                   # Request 2 GPU
 
@@ -13,4 +13,4 @@
 source /home/amyn/AIProject/bin/activate # Change "myenv" to your virtual environment name
 
 # Run your Python script
-python 3CNN_LSTM.py
+python 3CNN_fusion_3class_attention.py
